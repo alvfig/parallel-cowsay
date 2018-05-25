b29dbfcd78182b648a3b792d3cf9d2b0dd1465b4
Alvaro Figueiredo <alvaroaf+git@gmail.com>
2018-05-25 00:10:47 -0300

    Format the output of time and fix the count of Cores vs CPUs

--- run.sh
+++ run.sh
@@ -4,13 +4,15 @@ BAT="./batch.sh"
 RDS="3"
 
 lscpu
-AWKSCT='BEGIN{prvs=-1}{if(4<NR&&prvs!=$1){++cnt;prvs=$1}}END{print cnt}'
+AWKSCT='BEGIN{prvs=-1}{if(4<NR&&prvs!=$2){++cnt;prvs=$2}}END{print cnt}'
 CPUCNT=`lscpu -p | awk -F , "$AWKSCT"`
+CRECNT=`nproc`
 
 for i in `seq $RDS`; do
     echo "\n====> ROUND #$i <====\n"
-    xargs -I{} echo "env XARGS='{}' time $BAT > /dev/null" <<-ARGS | sh -v 2>&1
+    xargs -I{} echo "env XARGS='{}' time -p $BAT > /dev/null" <<-ARGS | sh -v 2>&1
     xargs -P 0
+    xargs -P $CRECNT
     xargs -P $CPUCNT
     parallel
     xargs -P 1
